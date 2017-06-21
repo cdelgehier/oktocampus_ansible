9e2e30cc8b182a0cf0f3c9f98cacb7e0ea0a6738
Delgehier Cédric <cedric.delgehier@gmail.com>
2017-06-21 22:31:36 +0200

    customize docker for non root user

--- bootstrap-bastion.sh
+++ bootstrap-bastion.sh
@@ -8,16 +8,30 @@ yum install -y python-pip gcc python-devel libffi-devel openssl-devel sshpass gi
 /usr/bin/pip install --upgrade setuptools
 /usr/bin/pip install ansible
 
+
+cat > /etc/docker/daemon.json << EOF
+{
+    "live-restore": true,
+    "group": "dockerroot"
+}
+EOF
+
+service docker start
+usermod -aG dockerroot vagrant
+docker pull bertvv/ansible-testing:centos_7
+docker pull hello-world
+
 # copy examples
 install -d -o vagrant -g vagrant -m 755 /home/vagrant/git/oktocampus
 cp -a /vagrant/examples/* /home/vagrant/git/oktocampus
 
 # configure hosts file for our internal network defined by Vagrantfile
-cat >> /etc/hosts <<EOL
+cat >> /etc/hosts <<EOF
+
 # vagrant environment nodes
 10.0.15.10  bastion
 10.0.15.21  prod1
 10.0.15.22  prod2
 10.0.15.51  rec1
 10.0.15.52  rec2
-EOL
+EOF
