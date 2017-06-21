bf88c9781f6569de1fddbf134ac620977db79cb8
Delgehier Cédric <cedric.delgehier@gmail.com>
2017-06-21 15:43:42 +0200

    add docker engine for tests

--- bootstrap-bastion.sh
+++ bootstrap-bastion.sh
@@ -3,7 +3,7 @@
 # Ansible installation
 yum install --enablerepo=extras epel-release -y
 
-yum install -y python-pip gcc python-devel libffi-devel openssl-devel sshpass git
+yum install -y python-pip gcc python-devel libffi-devel openssl-devel sshpass git docker
 /usr/bin/pip install --upgrade pip
 /usr/bin/pip install --upgrade setuptools
 /usr/bin/pip install ansible
