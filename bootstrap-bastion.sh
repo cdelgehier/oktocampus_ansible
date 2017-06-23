b64483bbf59a929a1d60538665d2f8a6e2138f1e
Delgehier Cédric <cedric.delgehier@gmail.com>
2017-06-24 01:50:30 +0200

    typo

--- bootstrap-bastion.sh
+++ bootstrap-bastion.sh
@@ -9,19 +9,18 @@ yum install -y python-pip gcc python-devel libffi-devel openssl-devel sshpass gi
 /usr/bin/pip install ansible
 
 
-<<<<<<< HEAD
+# DOCKER
 cat > /etc/docker/daemon.json << EOF
 {
     "live-restore": true,
     "group": "dockerroot"
 }
 EOF
-
 service docker start
 usermod -aG dockerroot vagrant
 docker pull bertvv/ansible-testing:centos_7
 docker pull hello-world
-=======
+
 #VIM
 install -d -o vagrant -g vagrant -m 750 /home/vagrant/.vim/{autoload,bundle}
 curl -LSso /home/vagrant/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -55,23 +54,18 @@ set scrolloff=3 " Minimum number of lines to show below/above the cursor
 
 " Theming
 colorscheme delek
-
 EOF
 su - vagrant -c "cd /home/vagrant/.vim/bundle && git clone https://github.com/pearofducks/ansible-vim && git clone https://github.com/Glench/Vim-Jinja2-Syntax"
 echo vi=vim >> /home/vagrant/.bashrc
->>>>>>> ed04c92326d4becc379c5658b34db30ab5d1ec10
 
-# copy examples
+# get examples on github
 install -d -o vagrant -g vagrant -m 755 /home/vagrant/git/oktocampus
 su - vagrant -c "git clone https://github.com/cdelgehier/oktocampus_ansible.git /home/vagrant/git/oktocampus"
 
 
 # configure hosts file for our internal network defined by Vagrantfile
 cat >> /etc/hosts <<EOF
-<<<<<<< HEAD
 
-=======
->>>>>>> ed04c92326d4becc379c5658b34db30ab5d1ec10
 # vagrant environment nodes
 10.0.15.10  bastion
 10.0.15.21  prod1
